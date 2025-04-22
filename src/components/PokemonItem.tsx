9ef0155110f90aa735777b2091c94d2104a5e172
pawelsawczenko <pawelsawczenko@gmail.com>
2025-04-22 13:57:11 +0300

    refactor favorite & compare btn

--- src/components/PokemonItem.tsx
+++ src/components/PokemonItem.tsx
@@ -1,9 +1,6 @@
-import { FaRegStar, FaBalanceScaleRight } from 'react-icons/fa'
-
-interface PokemonItemProps {
-  name: string
-  index: number
-}
+import { PokemonItemProps } from '../types'
+import { ButtonFavorite } from './ButtonFavorite'
+import { ButtonCompare } from './ButtonCompare'
 
 export const PokemonItem = ({ name, index }: PokemonItemProps) => {
   const pokemonIndex = index < 10 ? `#00${index}` : `#0${index}`
@@ -12,31 +9,14 @@ export const PokemonItem = ({ name, index }: PokemonItemProps) => {
     console.log(`${pokemonIndex} - ${name}`)
   }
 
-  const handleFavClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.stopPropagation()
-    console.log(`fav ${pokemonIndex} - ${name}`)
-  }
-
-  const handleCompareClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.stopPropagation()
-    console.log(`compare ${pokemonIndex} - ${name}`)
-  }
-
   return (
     <div
       className="w-xs mb-4 bg-amber-100 shadow-sm transition hover:bg-amber-200 hover:shadow-xl cursor-pointer"
       onClick={handleItemClick}>
       <div className="flex justify-end p-2">
-        <button className="mr-2 transition hover:text-red-800" onClick={(e) => handleFavClick(e)}>
-          {/* <FaStar /> */}
-          <FaRegStar />
-        </button>
+        <ButtonFavorite name={name} index={index} />
 
-        <button
-          className="mr-2 transition hover:text-blue-700"
-          onClick={(e) => handleCompareClick(e)}>
-          <FaBalanceScaleRight />
-        </button>
+        <ButtonCompare name={name} index={index} />
       </div>
       <div className="p-2">
         <h2>{pokemonIndex}</h2>
