bf35bc4505121625261443218956d9b51059ddcd
pawelsawczenko <pawelsawczenko@gmail.com>
2025-05-19 21:11:17 +0300

    remove comments

--- src/screens/PokemonComparison.tsx
+++ src/screens/PokemonComparison.tsx
@@ -1,47 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { PokemonComparisonItem } from '../components/PokemonComparisonItem'
-// import { PokemonData } from '../types'
 import { AppDispatch, RootState } from '../store/store'
 import { Error } from '../components/Error'
 import { Spinner } from '../components/Spinner'
 import { useEffect } from 'react'
 import { getPokemonComparisonDetails } from '../store/pokemonComparisonSlice'
-
-// const mockData: {
-//   pokemonLeft: PokemonData
-//   pokemonRight: PokemonData
-// } = {
-//   pokemonLeft: {
-//     index: '4',
-//     name: 'charmander',
-//     height: 6,
-//     weight: 85,
-//     img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
-//     stats: [
-//       { name: 'hp', value: 39 },
-//       { name: 'attack', value: 52 },
-//       { name: 'defense', value: 43 },
-//       { name: 'special-attack', value: 60 },
-//       { name: 'special-defense', value: 50 },
-//       { name: 'speed', value: 65 }
-//     ]
-//   },
-//   pokemonRight: {
-//     index: '7',
-//     name: 'squirtle',
-//     height: 5,
-//     weight: 90,
-//     img: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png',
-//     stats: [
-//       { name: 'hp', value: 44 },
-//       { name: 'attack', value: 48 },
-//       { name: 'defense', value: 65 },
-//       { name: 'special-attack', value: 50 },
-//       { name: 'special-defense', value: 64 },
-//       { name: 'speed', value: 43 }
-//     ]
-//   }
-// }
 
 export const PokemonComparison = () => {
   const isLoading = useSelector((state: RootState) => state.pokemonComparison.isLoading)
@@ -86,8 +49,6 @@ export const PokemonComparison = () => {
               <p>Try add more pokemons to comparison</p>
             </div>
           )}
-          {/* <PokemonComparisonItem comparedPokemons={comparedPokemons} pokemonToRender={0} />
-          <PokemonComparisonItem comparedPokemons={comparedPokemons} pokemonToRender={1} /> */}
         </div>
       )}
     </>
