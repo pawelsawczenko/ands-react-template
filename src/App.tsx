d67f40536260ea8e767b4169c2c8e35e3c93dcc6
pawelsawczenko <pawelsawczenko@gmail.com>
2025-05-22 19:47:53 +0300

    add redux-persist & persistence
    persistence to favorite and comparison slices

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import { Provider } from 'react-redux'
 import './index.css'
 import { RootNavigation } from './navigation/RootNavigation'
-import { store } from './store/store'
+import { persistor, store } from './store/store'
 import { Toaster } from 'react-hot-toast'
+import { PersistGate } from 'redux-persist/integration/react'
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <RootNavigation />
-      <Toaster
-        position="top-center"
-        gutter={12}
-        containerStyle={{ margin: '8px' }}
-        toastOptions={{
-          error: { duration: 3000 },
-          style: {
-            fontSize: '1rem',
-            maxWidth: '40rem',
-            padding: '1rem 1.5rem',
-            backgroundColor: 'lightgray',
-            color: 'black',
-            border: '1px solid red'
-          }
-        }}
-      />
+      <PersistGate loading={null} persistor={persistor}>
+        <RootNavigation />
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: '8px' }}
+          toastOptions={{
+            error: { duration: 3000 },
+            style: {
+              fontSize: '1rem',
+              maxWidth: '40rem',
+              padding: '1rem 1.5rem',
+              backgroundColor: 'lightgray',
+              color: 'black',
+              border: '1px solid red'
+            }
+          }}
+        />
+      </PersistGate>
     </Provider>
   )
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,47 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import pokemonListReducer from './pokemonListSlice'
 import pokemonDetailsReducer from './pokemonDetailsSlice'
 import pokemonFavoriteListReducer from './pokemonFavoriteListSlice'
 import pokemonComparisonReducer from './pokemonComparisonSlice'
+import storage from 'redux-persist/lib/storage'
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE
+} from 'redux-persist'
 
-const RootReducer = {
+const persistConfig = {
+  key: 'reactPokemon',
+  storage,
+  whitelist: ['pokemonFavoriteList', 'pokemonComparison'],
+  blacklist: ['pokemonList', 'pokemonDetails']
+}
+
+const RootReducer = combineReducers({
   pokemonList: pokemonListReducer,
   pokemonDetails: pokemonDetailsReducer,
   pokemonFavoriteList: pokemonFavoriteListReducer,
   pokemonComparison: pokemonComparisonReducer
-}
+})
+
+const persistedReducer = persistReducer(persistConfig, RootReducer)
 
 export const store = configureStore({
-  reducer: RootReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
