bcc56bade3582394a8b427e0057c22aa0e90725f
pawelsawczenko <pawelsawczenko@gmail.com>
2025-05-22 22:09:40 +0300

    remove comments

--- src/services/index.ts
+++ src/services/index.ts
@@ -33,6 +33,4 @@ export const pokemonListApi = createApi({
   })
 })
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
 export const { useGetPokemonInitialListQuery } = pokemonListApi
